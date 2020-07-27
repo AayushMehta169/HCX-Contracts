538f079e9c383361aa8e01f3b2aef5dca91513bf
Aayush Mehta <aayushmehta169@gmail.com>
2020-07-27 15:09:42 +0530

    init

---
+++ contracts/Election.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract Election {
+   struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    constructor () public {
+        addCandidate("Candidate 1/Yes");
+        addCandidate("Candidate 2/No");
+    }
+    mapping(uint => Candidate) public candidates;
+
+    mapping(address => bool) public voters;
+     uint public candidatesCount;
+
+
+      function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
+    }
+
+}
